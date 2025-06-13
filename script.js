950bcfba96d10ec11f2f5bee211577030bd6a7c5
Richard Darby <rkellindarby@gmail.com>
2025-06-12 18:02:09 -0800

    Created electric directory and electrical.html

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function setSpot(){
+/*function setSpot(){
 	let xSpot = document.getElementById("box1");
 	xSpot.innerHTML +="X";
 }
 
 let btnWidget = document.getElementById("button");
 btnWidget.addEventListener("click", setSpot);
+*/
+document.addEventListener('keydown', (event) => {
+	if(event.key === 's') {
+		console.log("s pressed");
+		document.getElementById("1").innerHTML = "X";
+	}
+});
